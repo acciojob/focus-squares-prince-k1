ce3abc41751c1df1f60915e9908bedd9a7777a40
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-21 09:20:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let box = document.querySelectorAll(".square");
+box.forEach((element) => {
+  element.addEventListener('mouseover' ,(event) => {
+    box.forEach((elem) => {
+      if(elem != element){
+        elem.style.backgroundColor = "#6F4E37";
+      }
+    })
+  })
+  element.addEventListener('mouseout' ,(event) => {
+    box.forEach((elem) => {
+      
+        elem.style.backgroundColor = "#E6E6FA";
+    })
+  })
+})
